7dcff8f2680d44f0ff38d063a4eda6105704ff87
andrecosta8 <andrecosta8@gmail.com>
2022-11-14 19:29:58 +0000

    all search endpoint logic, some design and test functionality

--- src/containers/Search/Search.jsx
+++ src/containers/Search/Search.jsx
@@ -3,53 +3,68 @@ import MovieDetail from "../../components/MovieDetail/MovieDetail";
 import { getSearchedMovies } from "../../services/apiCalls";
 import "./Search.css";
 
+
 const Search = () => {
   const [movies, setMovies] = useState([]);
   const [searchInput, setSearchInput] = useState("");
   const [selected, setSelected] = useState("");
 
 
-  const handleSearchInput = (e) => {
+const searchInputHandler = (e) => {
     setSearchInput((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
   };
 
-  useEffect(() => {
-    async function fecthData() {
-      let response = await getSearchedMovies(searchInput);
-      setMovies(response.data.results);
-    }
-    fecthData();
-    console.log(movies)
-  }, [searchInput]);
+useEffect(()=>{
+  const fecthData = async () => {
+    let response = await getSearchedMovies(searchInput);
+    let results = response.data.results
+    setMovies(results)
+}
+fecthData()
+},[searchInput])
+
+// const fecthData = async (e) => {
+//     e.preventDefault()
+//     let response = await getSearchedMovies(searchInput);
+//     let results = response.data.results
+//     setMovies(results)
+// }
 
   const selectMovie = (movie) => {
     setSelected(movie);
   };
 
+  
   return (
-  <div className="searchPage">
-    <div className="leftside">
-    <div className="searchBar">
-      <input className="searchInput" type="text" name="input" id="input" title="input"
-      onChange={(e) => {handleSearchInput(e) }}></input>
-    </div>
-    <div className="searchResults">
-    {movies.map((movie) => {
+    <div className="searchPage">
+      <div className="searchleftside">
+        <div className="searchBar">
+            <input className="searchInput" type="text" name="input" id="input" title="input" placeholder="Search your movie here"
+              onChange={(e) => { searchInputHandler(e) }}></input>
+        </div>
+        <div className="searchResults">
+          {movies.map((movie) => {
             return (
               <div className="movieCard" key={movie.id}>
-                <div >{movie.name}</div>
+              <div onClick={() => selectMovie(movie)}><img className="movieSmallImage" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title}/></div>
+                <div >{movie.title}</div> 
               </div>
             );
           })}
+        </div>
+      </div>
+      <div className="searchrightside">
+      {
+                selected?.id !== undefined &&
+                <MovieDetail movie={selected} />
+            }
+      </div>
     </div>
-    </div>
-    <div className="rightside">
-    </div>
-  </div>
 
-)};
+  )
+};
 
 export default Search;
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -9,11 +9,16 @@ export const getPopularMovies= async () => {
 }
 
 export const getSearchedMovies = async (query) => {
-        return await axios.get(`https://api.themoviedb.org/3/search/company?api_key=${API_KEY}&query=${query.input}&page=1`) 
+        return await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query.input}&page=1&include_adult=false`) 
+}
+
+export const getDetailsById = async (id) =>{
+        return await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US
+        `)
 }
 
 // Popular:
 // https://api.themoviedb.org/3/movie/popular?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1
 
 // Search:
-// https://api.themoviedb.org/3/search/movie?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&query=alien&page=1&include_adult=false
+//https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query.input}&page=1&include_adult=false
