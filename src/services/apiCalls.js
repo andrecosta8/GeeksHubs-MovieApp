a9289f4d41b24b81742d43f79d06aa12a713965f
andrecosta8 <98711382+andrecosta8@users.noreply.github.com>
2022-11-17 10:44:18 +0000

    Update apiCalls.js

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -16,9 +16,3 @@ export const getDetailsById = async (id) =>{
         return await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US
         `)
 }
-
-// Popular:
-// https://api.themoviedb.org/3/movie/popular?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1
-
-// Search:
-//https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query.input}&page=1&include_adult=false
